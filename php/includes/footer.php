4044f5af8a3b7583ea63dcf5a030655f2bc2eeb9
Boxon-BH <subscriptions@boxonvision.com>
2025-09-09 15:24:07 +0300

    updated user rating section

--- php/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if (is_arabic_page())	{ 
-    include "includes/footer-ar.php";
-} else {
-    include "includes/footer-en.php";
-}
-?>
--- php/includes/header.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if (is_arabic_page())	{ 
-    include "includes/header-ar.php";
-} else {
-    include "includes/header-en.php";
-}
-?>
